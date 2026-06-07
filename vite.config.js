c99f12a5aecddf1b7c54e73bcd1b7fadca557d7d
Assyrian91 <khoshabayalda91@gmail.com>
2026-06-07 09:31:42 +0300

    fix vite chunk config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          pdf: ['jspdf', 'html2canvas'],
+          email: ['@emailjs/browser'],
+        }
+      }
+    }
   }
 })
