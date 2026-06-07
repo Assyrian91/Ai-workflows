32df7fb92b33f414ef59e3c5fce53d1abceb7f92
Assyrian91 <khoshabayalda91@gmail.com>
2026-06-07 08:58:20 +0300

    optimise build chunks

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          pdf: ['jspdf', 'html2canvas'],
+          email: ['@emailjs/browser'],
+          redis: ['@upstash/redis'],
+        }
+      }
+    }
+  }
 })
