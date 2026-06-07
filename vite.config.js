35e4634ace8d0caad3a87487ca91b9325cbb75a2
Assyrian91 <khoshabayalda91@gmail.com>
2026-06-07 09:46:38 +0300

    revert vite config to original

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          pdf: ['jspdf', 'html2canvas'],
-          email: ['@emailjs/browser'],
-          redis: ['@upstash/redis'],
-        }
-      }
-    }
-  }
 })
